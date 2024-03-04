1a81ef7b78dbca1c411a5b740f8e49309ef87646
Theogene-dotcom <64300797+Theogene-dotcom@users.noreply.github.com>
2024-03-04 05:25:34 -0800

    Update views.py

--- lundi/views.py
+++ lundi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(request,'welcome to kigali coding')
+    return HttpResponse(request,'welcome to kigali coding.com')
