cbf8197da7368dff0042c37590443d1593d44643
Theogene-dotcom <64300797+Theogene-dotcom@users.noreply.github.com>
2024-03-04 09:56:35 -0800

    updated

--- lundi/settings.py
+++ lundi/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 
 
 # Default primary key field type
@@ -127,5 +127,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles'),
-STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS= (os.path.join(BASE_DIR,'staticfiles'),)
 django_heroku.settings(locals())
